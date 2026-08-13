d273a064d77619ada0577f324f7d74c868d2aadf
vugtqsvgv <vugtqsvgv@proton.me>
2026-08-13 16:29:01 +0100

    no blur no problem

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -83,7 +83,7 @@ hl.config({
         },
 
         blur = {
-            enabled   = true,
+            enabled   = false,
             size      = 3,
             passes    = 1,
             vibrancy  = 0.1696,
