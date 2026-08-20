3fa24a4126d24bf8803b575a5cac51185356cd68
vugtqsvgv <vugtqsvgv@proton.me>
2026-08-20 15:48:47 +0100

    neo vim is ass and i hate life because of it

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git add -f .config/hypr .config/rofi .config/quickshell .config/kitty .config/nvim .zshrc install.sh push.sh
+git add .config/hypr .config/rofi .config/quickshell .config/kitty .config/nvim/lua .zshrc install.sh push.sh
 
 git commit -m "$1"
 
