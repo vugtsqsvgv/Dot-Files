ce277d709b80989cc12006b758503d46710f895e
vugtqsvgv <vugtqsvgv@proton.me>
2026-08-21 08:49:56 +0100

    i hate vim maaaaan

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git add .config/hypr .config/rofi .config/quickshell .config/kitty .config/nvim/lua .config/yazi .zshrc install.sh push.sh
+git add .config/hypr .config/rofi .config/quickshell .config/kitty .config/nvim .config/yazi .zshrc install.sh push.sh
 
 git commit -m "$1"
 
