a462da0524b897cc9908f591e195e235dc456603
vugtqsvgv <vugtqsvgv@proton.me>
2026-08-26 11:55:07 +0100

    "zsh"

--- .config/kitty/scripts/launch/push-archIso.sh
+++ .config/kitty/scripts/launch/push-archIso.sh
@@ -8,4 +8,8 @@ git add baseline/
 
 git commit -m "$msg"
 
+git subtree pull --prefix=baseline/airootfs/etc/skel https://github.com/vugtsqsvgv/Dot-Files main
+
+git commit -m "$msg"
+
 git push -u origin main
